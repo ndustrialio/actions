f6833d1940d5be959df94d7d6f8619394b835a97
Mathew Elting <melting@ndustrial.io>
2023-06-15 11:36:45 -0400

    chore: remove temporary bypass of prod url.

--- get-tenants/index.js
+++ get-tenants/index.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env -S node --no-warnings
 
 const main = async (env = "") => {
-    env = "staging" //TODO remove when prod works
     const url = `https://nio-internal.api${env == 'prod'? "" : ".staging"}.ndustrial.io/graphql`;
 
     const response = await fetch(url, {
