445c512a2f5ceb8f282ccf05e6fcc5af1bb40a4d
Mathew Elting <melting@ndustrial.io>
2023-06-14 21:28:25 -0400

    chore: temporarily talk to staging internal API

--- get-tenants/index.js
+++ get-tenants/index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env -S node --no-warnings
 
 const main = async (env = "") => {
+    env = "staging" //TODO remove when prod works
     const url = `https://nio-internal.api${env == 'prod'? "" : ".staging"}.ndustrial.io/graphql`;
 
     const response = await fetch(url, {
